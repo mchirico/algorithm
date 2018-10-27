36513f509534130d2633e41db369cf7249b016f5
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-10-27 14:09:07 -0400

    Initial

---
+++ ios/Algorithm/Algorithm/Algorithm.swift
@@ -0,0 +1,32 @@
+//
+//  Algorithm.swift
+//  Algorithm
+//
+//  Created by Michael Chirico on 10/27/18.
+//  Copyright © 2018 Michael Chirico. All rights reserved.
+//
+
+
+public struct Stack<T> {
+  fileprivate var array = [T]()
+  
+  public var isEmpty: Bool {
+    return array.isEmpty
+  }
+  
+  public var count: Int {
+    return array.count
+  }
+  
+  public mutating func push(_ element: T) {
+    array.append(element)
+  }
+  
+  public mutating func pop() -> T? {
+    return array.popLast()
+  }
+  
+  public var top: T? {
+    return array.last
+  }
+}
---
+++ ios/Algorithm/AlgorithmTests/AlgorithmTests.swift
@@ -0,0 +1,51 @@
+//
+//  AlgorithmTests.swift
+//  AlgorithmTests
+//
+//  Created by Michael Chirico on 10/27/18.
+//  Copyright © 2018 Michael Chirico. All rights reserved.
+//
+
+import XCTest
+@testable import Algorithm
+
+class AlgorithmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+  
+  func testStack() {
+    var stack = Stack<Int>()
+    stack.push(3)
+    stack.push(10)
+    
+    XCTAssert(stack.count == 2)
+    XCTAssert(stack.pop() == 10)
+    XCTAssert(stack.pop() == 3)
+    XCTAssert(stack.isEmpty)
+    
+    
+    
+    
+  }
+  
+  
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
